991bfc6eecc26f1515dc624372fbe48352750638
Tontinke <Tontinke@10.14.234.120>
2012-11-04 12:05:45 +0200

    Harmadik commit

--- workspace/HelloWorld/src/MyClass.java
+++ workspace/HelloWorld/src/MyClass.java
@@ -1,6 +1,10 @@
 
 public class MyClass {
 	
+	/***
+	 * This is the main function.
+	 * @param args
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello world!");
 		System.out.println("Udv mindenki!");
