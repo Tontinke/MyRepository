686d512cc44394b3e80594ac5c10693bb6b5bad6
ktundek <ktundek@10.14.234.120>
2012-11-05 16:01:48 +0200

    Aktualis ido

--- workspace/HelloWorld/src/MyClass.java
+++ workspace/HelloWorld/src/MyClass.java
@@ -1,5 +1,8 @@
+import java.io.ObjectInputStream.GetField;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-public class MyClass {
+public class MyClass{
 	
 	/***
 	 * This is the main function.
@@ -8,6 +11,13 @@ public class MyClass {
 	public static void main(String[] args){
 		System.out.println("Hello world!");		
 		System.out.println("Udv mindenki!");
-		System.out.println("CSak egyszer tudnam mar felcommitolni!");
+		System.out.println("The current time is: "+ getCurrentTime());
+	}
+
+	public static String getCurrentTime() {
+		Calendar cal = Calendar.getInstance();
+		cal.getTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		return sdf.format(cal.getTime());		
 	}
 }
