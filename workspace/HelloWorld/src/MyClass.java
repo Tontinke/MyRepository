93083f89a035cc775df006cbfc56d98e868dd7bc
Tontinke <Tontinke@10.14.234.120>
2012-11-06 16:01:48 +0200

    kommentek torlese

--- workspace/HelloWorld/src/MyClass.java
+++ workspace/HelloWorld/src/MyClass.java
@@ -3,19 +3,11 @@ import java.util.Calendar;
 
 public class MyClass{
 	
-	/***
-	 * This is the main function.
-	 * @param args
-	 */
 	public static void main(String[] args){
 		System.out.println("Hello world!");	
 		System.out.println("The current time is: "+ getCurrentTime());		
 	}
 
-	/***
-	 * GEts the current time
-	 * @return string
-	 */
 	public static String getCurrentTime() {
 		Calendar cal = Calendar.getInstance();
 		cal.getTime();
