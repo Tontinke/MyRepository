8b0db7a2e60aa6525eddf836fab13dc00b1efc20
Tontinke <Tontinke@10.14.234.120>
2012-11-06 19:49:59 +0200

    utkozes

--- workspace/HelloWorld/src/MyClass.java
+++ workspace/HelloWorld/src/MyClass.java
@@ -10,7 +10,7 @@ public class MyClass{
 	public static void main(String[] args){
 		System.out.println("Hello world!");			
 		System.out.println("The current time is: "+ getCurrentTime());
-		System.out.println("");
+		System.out.println(" ");
 	}
 
 	/***
